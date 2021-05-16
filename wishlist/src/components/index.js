22fabf160d97e18f7d665020e23825c698d1d363
Roxane Martin <36088627+martinr9315@users.noreply.github.com>
2021-05-16 16:57:19 -0700

    Delete wishlist/src directory -- replaced with new framework

--- wishlist/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Navigation } from "./Navigation";
-export { default as Home } from "./Home";
-export { default as MyGroups } from "./MyGroups";
-export { default as CreateGroup } from "./CreateGroup";
-export { default as GroupView } from "./GroupView";
-export { default as MyList } from "./MyList";
-export { default as OtherList } from "./OtherList";
