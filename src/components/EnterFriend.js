a898913e985777cc64eec4b5c59f427d89dad77b
Justin Hee <justinhee118@gmail.com>
2021-05-16 19:45:42 -0700

    Added handleSubmit function framework to be called when a username is submitted.

--- src/components/EnterFriend.js
+++ src/components/EnterFriend.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 
 export default class EnterFriend extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { friends: []}
+    }
+    handleSubmit = event => {
+        event.preventDefault();
+        var username = document.getElementById("addFriend").username.value;
+        console.log(username);
+        console.log(this.state.friends)
+    } 
     render() {
         return (
             <div className="main">
                 <div className="mainDiv">
-                    <form>
+                    <form id="addFriend" onSubmit={this.handleSubmit.bind(this)}>
                         <h3>Add Friend</h3>
-                        <label for="username">Username:</label>
+                        <label htmlFor="username">Username: </label>
                         <input type="text" name="username">
                         </input>
-                        <input type="submit" value="Add"></input>
+                        <input type="submit" value="Submit"></input>
                     </form>
                 </div>
             </div>
