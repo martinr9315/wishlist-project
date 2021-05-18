26eaf7d0f02dd4a2c0240d88b5b15ba4c2af8c14
Justin <justinhogarth17@g.ucla.edu>
2021-05-18 13:59:54 -0700

    Friend Search Bar

--- src/pages/other-list.js
+++ src/pages/other-list.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from 'components/Menu';
-import List from 'components/List';
+import Friends from 'components/Friends';
 
 import './index.css';
 ReactDOM.render(<Menu />, document.getElementById('menu'),);
-ReactDOM.render(<List />, document.getElementById('list'),);
+ReactDOM.render(<Friends />, document.getElementById('friends'),);
 
 
-  
