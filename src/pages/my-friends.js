3ec2e950be854a81c63d0758cf4d0791fae40baa
Justin <justinhogarth17@g.ucla.edu>
2021-05-19 00:33:01 -0700

    Additional Friend Search Capabilities

--- src/pages/my-friends.js
+++ src/pages/my-friends.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from 'components/Menu';
-
 import './index.css';
+import Friends from 'components/Friends'
+
 ReactDOM.render(<Menu />, document.getElementById('menu'));
+ReactDOM.render(<Friends />, document.getElementById('friends'));
