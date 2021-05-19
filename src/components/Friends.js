9ee964b8f4d904a969e6e9186911809b80afba8c
Justin <justinhogarth17@g.ucla.edu>
2021-05-18 17:37:51 -0700

    Friend Search Functionality

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -7,16 +7,30 @@ class Friends extends Component {
         super(props);
 
         this.state = {
-            friends: []
+            currentFriends: ['Bob', 'Sam', 'Tim']
           };
-     
+
+        this.friendSearch = this.friendSearch.bind(this);
+    }
+
+    friendSearch(e) {
+        if (this.state.currentFriends.includes(this._inputElement.value) === false)
+        {
+            alert('Friend not Present');
+            e.preventDefault();
+        }
+        else
+        {
+            alert('Friend Present! Here is their list.');
+            {/* Get friend list and place it here */};
+        }
     }
 
     render() {
         return (
             <div className="friends">
                 <div className="header">
-                    <form>{/*onSubmit={this.addItem}*/}
+                    <form onSubmit={this.friendSearch}>
                         <input ref={(a) => this._inputElement = a}
                                placeholder="Search for Friend">
                         </input>
