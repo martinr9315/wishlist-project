4dcc34e8768bae897c3d8fddddec2773685f78f6
Roxane Martin <roxane.a.martin@gmail.com>
2021-05-24 22:54:20 -0700

    linked login and dashboard so that all functionality is testable

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,9 +6,10 @@ export default class Menu extends Component {
     return (
       <div className={style.Menu}>
       <ul>
-        <li><a href="/index.html">Home</a></li>
+        <li><a href="/dashboard.html">Home</a></li>
         <li><a href="/my-list.html">My List</a></li>
         <li><a href="/my-friends.html">My Friends</a></li>
+        <li><a href="/index.html">Back to Login</a></li>
       </ul>
       </div>
     );
