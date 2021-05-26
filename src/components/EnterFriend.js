b7d2d980eb5dbd52221d6890ed00a77299d5f56a
Justin Hee <justinhee118@gmail.com>
2021-05-25 21:54:46 -0700

    added display of friends list, and remove friend

--- src/components/EnterFriend.js
+++ src/components/EnterFriend.js
@@ -8,10 +8,35 @@ export default class EnterFriend extends Component {
     handleSubmit = event => {
         event.preventDefault();
         var username = document.getElementById("addFriend").username.value;
-        console.log(username);
-        console.log(this.state.friends);
+        let friends = this.state.friends;
+        if(friends.includes(username)){
+            alert(username + " already added!");
+        } else {
+            friends.push(username);
+            this.setState({
+                friends:friends
+            });
+            console.log(username + " added");
+            console.log(this.state.friends);
+        }
     } 
+
+    removeFriend(friend) {
+        this.setState({
+            friends: this.state.friends.filter(function(username) {
+                return username !== friend;
+            })
+        })
+    }
+
     render() {
+        const friends = this.state.friends;
+            const currentFriends = friends.map((username) => 
+                    <li key={username}>
+                        {username}
+                        <button onClick={() => this.removeFriend(username)}>Remove</button>
+                    </li>
+            );
         return (
             <div className="main">
                 <div className="mainDiv">
@@ -22,6 +47,9 @@ export default class EnterFriend extends Component {
                         </input>
                         <input type="submit" value="Submit"></input>
                     </form>
+                </div>
+                <div className="friend-list">
+                    <ul>{currentFriends}</ul>
                 </div>
             </div>
         )
