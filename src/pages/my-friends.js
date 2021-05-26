1ac7c1db9b6434a255df372216fe5763cf2d9c7f
Justin <justinhogarth17@g.ucla.edu>
2021-05-25 18:06:25 -0700

    Merge conflicts resolved.

--- src/pages/my-friends.js
+++ src/pages/my-friends.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Menu from 'components/Menu';
+import FriendSearch from 'components/SearchFriendList'
 import './index.css';
 
 ReactDOM.render(<Menu />, document.getElementById('menu'));
+ReactDOM.render(<FriendSearch />, document.getElementById('friend-search'));
