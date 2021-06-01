d9844457f31322b4fbccdebfe170e686e022366e
camacju <75220173+camacju@users.noreply.github.com>
2021-06-01 18:28:41 -0500

    Delete src/pages directory

--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import Login from "components/login-menu";
-import "./index.css";
-
-ReactDOM.render(<Login />, document.getElementById("login"));
